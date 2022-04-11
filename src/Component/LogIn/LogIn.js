9f64f20b56f4d7ec6eac965543037cea7db86b38
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-11 21:57:51 +0600

    login condition added

--- src/Component/LogIn/LogIn.js
+++ src/Component/LogIn/LogIn.js
@@ -1,20 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const LogIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const handaleEmail = e => {
+        setEmail(e.target.value);
+    }
+    const handalePassword = e => {
+        setPassword(e.target.value)
+    }
+    const navigate = useNavigate();
+    if (user) {
+        navigate('/home')
+
+    }
+    const handaleUserSignIn = e => {
+        e.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
     return (
         <div className='form-container'>
             <div>
-                <form>
+                <form onSubmit={handaleUserSignIn}>
                     <h2 className='form-title'>Log in</h2>
                     <div className="inputs-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input onBlur={handaleEmail} type="email" name="email" id="" />
                     </div>
                     <div className="inputs-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input onBlur={handalePassword} type="password" name="password" id="" />
+                        <p style={{ color: 'red' }}>{error?.message}</p>
+                        {
+                            loading && <p>loading....</p>
+                        }
                     </div>
                     <input className='form-submit' type="submit" value="Log in" />
                 </form>
