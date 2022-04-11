7906cbed9015e7dd5e509042bc74d12f12e3228f
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-04-11 13:31:49 +0600

    signup created

--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>Sign up please</h2>
+        <div className='form-container'>
+            <div>
+                <form>
+                    <h2 className='form-title'>Sign up</h2>
+                    <div className="inputs-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" />
+                    </div>
+                    <div className="inputs-group">
+                        <label htmlFor="password">New Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <div className="inputs-group">
+                        <label htmlFor="password">Conform Password</label>
+                        <input type="password" name="password" id="" />
+                    </div>
+                    <input className='form-submit' type="submit" value="Submit" />
+                </form>
+                <p className='login-signup'>
+                    Already have an account? <Link className='form-link' to="/login">log in</Link>
+                </p>
+                <div className='hr-line'>
+                    <hr /> or <hr />
+                </div>
+                <div className='icon-group'>
+                    <button className='external-link'>Continue with google</button>
+                    <button className='external-link'>Continue with facebook</button>
+                    <button className='external-link'>Continue with microsoft</button>
+                </div>
+
+            </div>
         </div>
     );
 };
-
 export default SignUp;
